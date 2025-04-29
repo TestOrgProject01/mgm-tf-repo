827965a83fdcca8ef1194618e56c9421511ff7d2
pranay-mgm <pmadhavaram@mgmresorts.com>
2025-04-29 19:02:01 +0530

    Update provider.tf

--- test-app/scripts/provider.tf
+++ test-app/scripts/provider.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
-  oidc_request_token = "fghgfgkjkjfgkjlfklgklklfgklkfg"
+ 
   features {}
-
+  subscription_id = "your-subscription-id"
+  client_id       = "your-client-id"
+  client_secret   = "example@secret"
+  tenant_id       = "ABCIJDER"
+  oidc_request_token = "fghgfgkjkjfgkjlfklgklklfgklkfg"
 }
