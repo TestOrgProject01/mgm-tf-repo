5ee8c4c22f0d7668605849ac4d614d21a7bf94cf
pranay-mgm <pmadhavaram@mgmresorts.com>
2025-05-06 17:34:14 +0530

    Update provider.tf

--- poc-pin-reset/provider.tf
+++ poc-pin-reset/provider.tf
@@ -1,3 +1,9 @@
 provider "azurerm" {
   features {}
+
+  client_id                   = "00000000-0000-0000-0000-000000000000"
+  client_certificate_path     = "uhdfhdsufgdsufdfdf"
+  client_certificate_password = "sdkfsdfhsdgfiusdkfwo8fuewjfh"
+  tenant_id                   = "10000000-0000-0000-0000-000000000000"
+  subscription_id             = "20000000-0000-0000-0000-000000000000"
 }
