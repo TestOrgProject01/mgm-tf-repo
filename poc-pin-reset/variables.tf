8a3e0ec52cfe0e3f3a6d672e034587c8f2ec31db
pranay-mgm <pmadhavaram@mgmresorts.com>
2025-05-06 16:30:53 +0530

    Add files via upload

---
+++ poc-pin-reset/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {}
+variable "storage_account_name" {}
+variable "container_name" {}
+variable "key" {}
+variable "tags" {}
+variable "environment" {}
+variable "location" {}
+
+variable "project_prefix" {}
+variable "project_suffix" {}
